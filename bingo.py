8e49db4aa96d3b25d6a636c18527922ba9e38900
Jaya Sahithi Konda <jskonda@Jayas-MacBook-Pro.local>
2021-12-11 10:54:57 -0800

    Generating tickets

---
+++ bingo.py
@@ -0,0 +1,31 @@
+def bingo():
+    import random
+
+    n = 3
+    for n1 in range(n):
+        tkt = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+        d = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
+
+        l = []
+        while 1 < 2:
+            a = random.randint(1, 89)
+
+            if a not in l:
+                if d[int(a / 10)] < 3:
+                    l.append(a)
+                    d[int(a / 10)] += 1
+                    if len(l) == 15:
+                        break
+
+        for x in l:
+            x1 = int(x / 10)
+            while 1 < 2:
+                a = random.randint(0, 2)
+                if tkt[a][x1] == 0:
+                    tkt[a][x1] = x
+                    break
+
+bingo()
